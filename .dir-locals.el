017a077192723b309d91b35485440ee11baabee6
Ellery <ellery92@gmail.com>
2018-09-18 09:15:47 +0800

    initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . (
+		 (eval . (setq cmake-ide-project-dir
+                       (expand-file-name "~/workspace/project/lycam-c")))
+		 (eval . (setq cmake-ide-build-dir
+                       (expand-file-name "~/workspace/project/lycam-c/build")))
+         )))
